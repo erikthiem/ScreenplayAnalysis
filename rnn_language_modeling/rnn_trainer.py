7519060f4b26745f2aa45679e7f9e4eb473963f3
sydneyhodge <sydneyahodge@gmail.com>
2016-04-24 12:27:24 -0400

    Removed unneed Data initialization line.

--- rnn_language_modeling/rnn_trainer.py
+++ rnn_language_modeling/rnn_trainer.py
@@ -7,8 +7,7 @@ from Preprocess import ScreenplayData
 
 class RNN_Trainer:
 	def __init__(self, inFile):
-		self.train = Data.__init__(self, inFile)
-
+		
 if __name__ == "__main__":
 	vocab_size = sys.argv[1] if len(sys.argv) > 1 else 8000
 	train = ScreenplayData("../data/processed/lines_by_genre.db", vocab_size)
